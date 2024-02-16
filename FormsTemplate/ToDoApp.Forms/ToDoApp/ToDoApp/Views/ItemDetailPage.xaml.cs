6771c9d0439a9a86ca488017ff0ef81f1bbc7cb3
Dan Ardelean <dan.ardelean@mahiz.it>
2024-02-16 08:36:11 +0100

    Added the old Xamarin.Forms template

---
+++ FormsTemplate/ToDoApp.Forms/ToDoApp/ToDoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using ToDoApp.ViewModels;
+using Xamarin.Forms;
+
+namespace ToDoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ FormsTemplate/ToDoApp.Forms/ToDoApp/ToDoApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using ToDoApp.Models;
+using ToDoApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
